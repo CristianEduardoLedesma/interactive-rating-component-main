f3c1b2b344d413092988c30078532389357ad485
CristianEduardoLedesma <87340217+CristianEduardoLedesma@users.noreply.github.com>
2022-08-27 13:22:10 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,12 +3,15 @@ const thankyouContainer = document.querySelector(".container-thank-you");
 
 const btnSubmit = document.querySelector(".btn-submit");
 
-const rating = document.querySelector(".rating");
+const rating = document.getElementById('rating');
 const btnNumbers = document.querySelectorAll(".btn-number");
 
 btnSubmit.addEventListener("click", () => {
+    if(rating.textContent === '') 
+    	return alert('Choose a rating before submitting!')
 	thankyouContainer.classList.remove("hidden")
 	ratingContainer.style.display = "none"
+	console.log(rating.innerHTML)
 })
 
 btnNumbers.forEach((btnNumber) => {
